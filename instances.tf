7bfb2f25615920a53188a3dd4b14be5cdd5c9f48
amena-aporna <amena.aporna@gmail.com>
2022-12-07 15:47:39 +0600

    uncmnt

--- instances.tf
+++ instances.tf
@@ -1,96 +1,96 @@
-# # Creating an AWS instance Public for the Webserver! zz
-# resource "aws_instance" "webserver" {
+# Creating an AWS instance Public for the Webserver! zz
+resource "aws_instance" "webserver" {
 
-#   depends_on = [
-#     aws_vpc.main,
-#     aws_subnet.public_us_east_1a,
-#     aws_subnet.public_us_east_1b,
-#     aws_subnet.private_us_east_1a,
-#     aws_subnet.private_us_east_1b,
-#     aws_security_group.BH-SG,
-#     aws_security_group.DB-SG-SSH
-#   ]
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b,
+    aws_subnet.private_us_east_1a,
+    aws_subnet.private_us_east_1b,
+    aws_security_group.BH-SG,
+    aws_security_group.DB-SG-SSH
+  ]
 
-#   # AMI ID 
-#   ami  = var.ami
-#   instance_type = "t2.micro"
-#   subnet_id = aws_subnet.public_us_east_1a.id
+  # AMI ID 
+  ami  = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_us_east_1a.id
 
-#   key_name = aws_key_pair.dbkp.key_name
-
-  
-#   //key_name = "dbkp_tf"
-  
-#   # Security groups to use!
-#   vpc_security_group_ids = [aws_security_group.WS-SG.id]
-
-#   tags = {
-#    Name = "Webserver_From_Terraform"
-#   }
-
-#   user_data = <<EOF
-#     #!/bin/bash
-#      yum update -y
-# 		 yum install -y httpd
-# 		 systemctl start httpd
-# 		 systemctl enable httpd
-# 		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-# 	EOF
-
-# }
-
-
-
-# # Creating an AWS instance Private for the MySQL! 
-# resource "aws_instance" "MySQL" {
-#   depends_on = [
-#     aws_instance.webserver,
-#   ]
-
-
-#   ami  = var.ami
-#   instance_type = "t2.micro"
-#   subnet_id = aws_subnet.private_us_east_1a.id
-
-#   key_name = aws_key_pair.dbkp.key_name
+  key_name = aws_key_pair.dbkp.key_name
 
   
-#   //key_name = "dbkp_tf"
-
-#   # Attaching 2 security groups here, 1 for the MySQL Database access by the Web-servers,
-#   # & other one for the Bastion Host access for applying updates & patches!
-#   vpc_security_group_ids = [aws_security_group.MySQL-SG.id, aws_security_group.DB-SG-SSH.id]
-
-#   tags = {
-#    Name = "MySQL_Terraform"
-#   }
-# }
-
-
-# # Creating an AWS instance for the Bastion Host, It should be launched in the public Subnet!
-# resource "aws_instance" "Bastion-Host" {
-#    depends_on = [
-#     aws_instance.webserver,
-#      aws_instance.MySQL,
-#      #aws_subnet.public_us_east_1a,
-#      #aws_subnet.public_us_east_1b
-#   ]
+  //key_name = "dbkp_tf"
   
-#   ami  = var.ami
-#   instance_type = "t2.micro"
-#   subnet_id = aws_subnet.public_us_east_1a.id
+  # Security groups to use!
+  vpc_security_group_ids = [aws_security_group.WS-SG.id]
 
-#   key_name = aws_key_pair.dbkp.key_name
+  tags = {
+   Name = "Webserver_From_Terraform"
+  }
+
+  user_data = <<EOF
+    #!/bin/bash
+     yum update -y
+		 yum install -y httpd
+		 systemctl start httpd
+		 systemctl enable httpd
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
+
+}
+
+
+
+# Creating an AWS instance Private for the MySQL! 
+resource "aws_instance" "MySQL" {
+  depends_on = [
+    aws_instance.webserver,
+  ]
+
+
+  ami  = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.private_us_east_1a.id
+
+  key_name = aws_key_pair.dbkp.key_name
+
+  
+  //key_name = "dbkp_tf"
+
+  # Attaching 2 security groups here, 1 for the MySQL Database access by the Web-servers,
+  # & other one for the Bastion Host access for applying updates & patches!
+  vpc_security_group_ids = [aws_security_group.MySQL-SG.id, aws_security_group.DB-SG-SSH.id]
+
+  tags = {
+   Name = "MySQL_Terraform"
+  }
+}
+
+
+# Creating an AWS instance for the Bastion Host, It should be launched in the public Subnet!
+resource "aws_instance" "Bastion-Host" {
+   depends_on = [
+    aws_instance.webserver,
+     aws_instance.MySQL,
+     #aws_subnet.public_us_east_1a,
+     #aws_subnet.public_us_east_1b
+  ]
+  
+  ami  = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_us_east_1a.id
+
+  key_name = aws_key_pair.dbkp.key_name
   
   
-#   //key_name = "MyKeyFinal"
+  //key_name = "MyKeyFinal"
    
-#   # Security group ID's
-#   vpc_security_group_ids = [aws_security_group.BH-SG.id]
-#   tags = {
-#    Name = "Bastion_Host_Terraform"
-#   }
-# }
+  # Security group ID's
+  vpc_security_group_ids = [aws_security_group.BH-SG.id]
+  tags = {
+   Name = "Bastion_Host_Terraform"
+  }
+}
 
 
 
--- security groups.tf
+++ security groups.tf
@@ -1,165 +1,166 @@
-# #Creating a Security Group for WordPress 
-# resource "aws_security_group" "WS-SG" {
+#Creating a Security Group for WordPress 
+resource "aws_security_group" "WS-SG" {
 
-#   depends_on = [
-#     aws_vpc.main,
-#     aws_subnet.public_us_east_1a,
-#     aws_subnet.public_us_east_1b,
-#     aws_subnet.private_us_east_1a,
-#     aws_subnet.private_us_east_1b
-#   ]
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b,
+    aws_subnet.private_us_east_1a,
+    aws_subnet.private_us_east_1b
+  ]
 
-#   description = "HTTP, PING, SSH"
+  description = "HTTP, PING, SSH"
 
-#   # Name of the security Group!
-#   name = "webserver-sg"
+  # Name of the security Group!
+  name = "webserver-sg"
   
-#   # VPC ID in which Security group has to be created!
-#   vpc_id = aws_vpc.main.id
+  # VPC ID in which Security group has to be created!
+  vpc_id = aws_vpc.main.id
 
-#   # Created an inbound rule for webserver access!
-#   ingress {
-#     description = "HTTP for webserver"
-#     from_port   = 80
-#     to_port     = 80
+  # Created an inbound rule for webserver access!
+  ingress {
+    description = "HTTP for webserver"
+    from_port   = 80
+    to_port     = 80
 
-#     # Here adding tcp instead of http, because http in part of tcp only!
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+    # Here adding tcp instead of http, because http in part of tcp only!
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   # Created an inbound rule for ping
-#   ingress {
-#     description = "Ping"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "ICMP"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  # Created an inbound rule for ping
+  ingress {
+    description = "Ping"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "ICMP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   # Created an inbound rule for SSH
-#   ingress {
-#     description = "SSH"
-#     from_port   = 22
-#     to_port     = 22
+  # Created an inbound rule for SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
 
-#     # Here adding tcp instead of ssh, because ssh in part of tcp only!
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+    # Here adding tcp instead of ssh, because ssh in part of tcp only!
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   # Outward Network Traffic for the WordPress
-#   egress {
-#     description = "output from webserver"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
-# # Creating security group for MySQL
-# resource "aws_security_group" "MySQL-SG" {
-
-#   depends_on = [
-#     aws_vpc.main,
-#     aws_subnet.public_us_east_1a,
-#     aws_subnet.public_us_east_1b,
-#     aws_subnet.private_us_east_1a,
-#     aws_subnet.private_us_east_1b,
-#     aws_security_group.WS-SG
-#   ]
-
-#   description = "MySQL Access only from the Webserver Instances!"
-#   name = "mysql-sg"
-#   vpc_id = aws_vpc.main.id
-
-#   # Created an inbound rule for MySQL
-#   ingress {
-#     description = "MySQL Access"
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     security_groups = [aws_security_group.WS-SG.id]
-#   }
-
-#   egress {
-#     description = "output from MySQL"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  # Outward Network Traffic for the WordPress
+  egress {
+    description = "output from webserver"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
+# Creating security group for MySQL
+resource "aws_security_group" "MySQL-SG" {
 
-# # Creating security group for Bastion Host/Jump Box
-# resource "aws_security_group" "BH-SG" {
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b,
+    aws_subnet.private_us_east_1a,
+    aws_subnet.private_us_east_1b,
+    aws_security_group.WS-SG
+  ]
 
-#   depends_on = [
-#     aws_vpc.main,
-#     aws_subnet.public_us_east_1a,
-#     aws_subnet.public_us_east_1b,
-#     aws_subnet.private_us_east_1a,
-#     aws_subnet.private_us_east_1b,
-#   ]
+  description = "MySQL Access only from the Webserver Instances!"
+  name = "mysql-sg"
+  vpc_id = aws_vpc.main.id
 
-#   description = "MySQL Access only from the Webserver Instances!"
-#   name = "bastion-host-sg"
-#   vpc_id = aws_vpc.main.id
+  # Created an inbound rule for MySQL
+  ingress {
+    description = "MySQL Access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.WS-SG.id]
+  }
 
-#   # Created an inbound rule for Bastion Host SSH
-#   ingress {
-#     description = "Bastion Host SG"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     description = "output from Bastion Host"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    description = "output from MySQL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
-# # Creating security group for MySQL Bastion Host Access
-# resource "aws_security_group" "DB-SG-SSH" {
+# Creating security group for Bastion Host/Jump Box
+resource "aws_security_group" "BH-SG" {
 
-#   depends_on = [
-#     aws_vpc.main,
-#     aws_subnet.public_us_east_1a,
-#     aws_subnet.public_us_east_1b,
-#     aws_subnet.private_us_east_1a,
-#     aws_subnet.private_us_east_1b,
-#     aws_security_group.BH-SG
-#   ]
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b,
+    aws_subnet.private_us_east_1a,
+    aws_subnet.private_us_east_1b,
+  ]
 
-#   description = "MySQL Bastion host access for updates!"
-#   name = "mysql-sg-bastion-host"
-#   vpc_id = aws_vpc.main.id
+  description = "MySQL Access only from the Webserver Instances!"
+  name = "bastion-host-sg"
+  vpc_id = aws_vpc.main.id
 
-#   # Created an inbound rule for MySQL Bastion Host
-#   ingress {
-#     description = "Bastion Host SG"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     security_groups = [aws_security_group.BH-SG.id]
-#   }
+  # Created an inbound rule for Bastion Host SSH
+  ingress {
+    description = "Bastion Host SG"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     description = "output from MySQL BH"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    description = "output from Bastion Host"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+# Creating security group for MySQL Bastion Host Access
+resource "aws_security_group" "DB-SG-SSH" {
+
+  depends_on = [
+    aws_vpc.main,
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b,
+    aws_subnet.private_us_east_1a,
+    aws_subnet.private_us_east_1b,
+    aws_security_group.BH-SG
+  ]
+
+  description = "MySQL Bastion host access for updates!"
+  name = "mysql-sg-bastion-host"
+  vpc_id = aws_vpc.main.id
+
+  # Created an inbound rule for MySQL Bastion Host
+  ingress {
+    description = "Bastion Host SG"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.BH-SG.id]
+  }
+
+  egress {
+    description = "output from MySQL BH"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
