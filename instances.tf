7bdecd39ba22f6ae22eeeea1bc459d0d5a814323
amena-aporna <amena.aporna@gmail.com>
2022-12-07 14:03:17 +0600

    typo fix

--- instances.tf
+++ instances.tf
@@ -28,8 +28,15 @@ resource "aws_instance" "webserver" {
    Name = "Webserver_From_Terraform"
   }
 
-  user_data = file("install_apache.sh")
-  
+  user_data = <<EOF
+    #!/bin/bash
+    sudo apt-get update
+		sudo apt-get install -y apache2
+		sudo systemctl start apache2
+		sudo systemctl enable apache2
+		echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+	EOF
+
 }
 
 
