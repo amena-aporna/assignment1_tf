2d32ea8edaa13511c5b80f5f8b56e5b1babdb433
amena-aporna <amena.aporna@gmail.com>
2022-12-08 11:53:35 +0600

    traget id

--- alb.tf
+++ alb.tf
@@ -2,72 +2,72 @@
 
 
 
-resource "aws_security_group" "ec2_eg1" {
-  name   = "ec2-eg1"
-  vpc_id = aws_vpc.main.id
-}
+# resource "aws_security_group" "ec2_eg1" {
+#   name   = "ec2-eg1"
+#   vpc_id = aws_vpc.main.id
+# }
 
-resource "aws_security_group" "alb_eg1" {
-  name   = "alb-eg1"
-  vpc_id = aws_vpc.main.id
-}
-
-
-resource "aws_security_group_rule" "ingress_ec2_traffic" {
-  type                     = "ingress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.ec2_eg1.id
-  source_security_group_id = aws_security_group.alb_eg1.id
-}
-
-resource "aws_security_group_rule" "ingress_ec2_health_check" {
-  type                     = "ingress"
-  from_port                = 8081
-  to_port                  = 8081
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.ec2_eg1.id
-  source_security_group_id = aws_security_group.alb_eg1.id
-}
-
-resource "aws_security_group_rule" "full_egress_ec2" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.ec2_eg1.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+ resource "aws_security_group" "alb_eg1" {
+   name   = "alb-eg1"
+   vpc_id = aws_vpc.main.id
+ }
 
 
+# resource "aws_security_group_rule" "ingress_ec2_traffic" {
+#   type                     = "ingress"
+#   from_port                = 8080
+#   to_port                  = 8080
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.ec2_eg1.id
+#   source_security_group_id = aws_security_group.alb_eg1.id
+# }
 
-resource "aws_security_group_rule" "ingress_alb_traffic" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = aws_security_group.alb_eg1.id
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "ingress_ec2_health_check" {
+#   type                     = "ingress"
+#   from_port                = 8081
+#   to_port                  = 8081
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.ec2_eg1.id
+#   source_security_group_id = aws_security_group.alb_eg1.id
+# }
 
-resource "aws_security_group_rule" "egress_alb_traffic" {
-  type                     = "egress"
-  from_port                = 8080
-  to_port                  = 8080
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.alb_eg1.id
-  source_security_group_id = aws_security_group.ec2_eg1.id
-}
+# resource "aws_security_group_rule" "full_egress_ec2" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   security_group_id = aws_security_group.ec2_eg1.id
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
-resource "aws_security_group_rule" "egress_alb_health_check" {
-  type                     = "egress"
-  from_port                = 8081
-  to_port                  = 8081
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.alb_eg1.id
-  source_security_group_id = aws_security_group.ec2_eg1.id
-}
+
+
+# resource "aws_security_group_rule" "ingress_alb_traffic" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   security_group_id = aws_security_group.alb_eg1.id
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
+
+# resource "aws_security_group_rule" "egress_alb_traffic" {
+#   type                     = "egress"
+#   from_port                = 8080
+#   to_port                  = 8080
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.alb_eg1.id
+#   source_security_group_id = aws_security_group.ec2_eg1.id
+# }
+
+# resource "aws_security_group_rule" "egress_alb_health_check" {
+#   type                     = "egress"
+#   from_port                = 8081
+#   to_port                  = 8081
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.alb_eg1.id
+#   source_security_group_id = aws_security_group.ec2_eg1.id
+# }
 
 
 # resource "aws_instance" "my_app_eg1" {
@@ -85,74 +85,67 @@ resource "aws_security_group_rule" "egress_alb_health_check" {
 #   }
 # }
 
-
-resource "aws_lb_target_group" "my_app_eg1" {
-  name       = "my-app-eg1"
-  port       = 8080
+#create target group
+resource "aws_lb_target_group" "front" {
+  name       = "application-front"
+  port       = 80
   protocol   = "HTTP"
   vpc_id     = aws_vpc.main.id
-  slow_start = 0
+  # slow_start = 0
 
-  load_balancing_algorithm_type = "round_robin"
+  # load_balancing_algorithm_type = "round_robin"
 
-  stickiness {
-    enabled = false
-    type    = "lb_cookie"
-  }
+  # stickiness {
+  #   enabled = false
+  #   type    = "lb_cookie"
+  # }
 
   health_check {
     enabled             = true
-    port                = 8081
+    port                = "traffic port"
     interval            = 30
     protocol            = "HTTP"
-    path                = "/health"
-    matcher             = "200"
+    path                = "/"
+    matcher             = 200
     healthy_threshold   = 3
     unhealthy_threshold = 3
   }
 }
 
-
-resource "aws_lb_target_group_attachment" "altga" {
-  depends_on = [
-     aws_instance.webserver,
-     aws_instance.MySQL,
-     #aws_subnet.public_us_east_1a,
-     #aws_subnet.public_us_east_1b
-  ]
-  target_group_arn = aws_lb_target_group.my_app_eg1.arn
-  target_id        = aws_vpc.main.id
-  port             = 8080
+# attaching target group to instances
+resource "aws_lb_target_group_attachment" "attach-app1" {
+  target_group_arn = aws_lb_target_group.front.arn
+  target_ids        = [aws_instance.webserver,aws_instance.MySQL]
+  port             = 80
 }
 
 
-resource "aws_lb" "al" {
-  name               = "al"
+#creating load balancer
+resource "aws_lb" "front" {
+  name               = "front"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_eg1.id]
 
-  # access_logs {
-  #   bucket  = "my-logs"
-  #   prefix  = "my-app-lb"
-  #   enabled = true
-  # }
-
   subnets = [
-    aws_subnet.public_us_east_1a.id,
-    aws_subnet.public_us_east_1b.id
+    aws_subnet.public_us_east_1a,
+    aws_subnet.public_us_east_1b
   ]
+  enable_deletion_protection = true 
+  
 }
 
+ 
 
-resource "aws_lb_listener" "http_eg1" {
-  load_balancer_arn = aws_lb.al.arn
+# listener checks for connection requests
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front.arn
   port              = "80"
   protocol          = "HTTP"
 
-  default_action {
+  default_action { 
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my_app_eg1.arn
+    target_group_arn = aws_lb_target_group.front.arn
   }
 }
 
