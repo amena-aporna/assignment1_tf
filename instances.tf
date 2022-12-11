ca986649187bd91e684220522ca05817a476a0bd
amena-aporna <amena.aporna@gmail.com>
2022-12-11 10:36:20 +0600

    removing bashtion host

--- instances.tf
+++ instances.tf
@@ -1,4 +1,4 @@
-# Creating an AWS instance Public for the Webserver! zzz
+# Creating an AWS instance Public for the Webserver!
 resource "aws_instance" "webserver" {
 
   depends_on = [
@@ -7,16 +7,15 @@ resource "aws_instance" "webserver" {
     aws_subnet.public_us_east_1b,
     aws_subnet.private_us_east_1a,
     aws_subnet.private_us_east_1b,
-    aws_security_group.BH-SG,
-    aws_security_group.DB-SG-SSH
   ]
+
 
   # AMI ID 
   ami  = var.ami
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public_us_east_1a.id
 
-  key_name = aws_key_pair.skp.key_name
+  key_name = aws_key_pair.kp.key_name
 
   
   # Security groups to use!
@@ -24,7 +23,7 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address = true 
 
   tags = {
-   Name = "Webserver_From_Terraform"
+   Name = "Webserver_Terraform"
   }
 
   user_data = <<EOF
@@ -55,57 +54,17 @@ resource "aws_instance" "MySQL" {
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private_us_east_1a.id
 
-  key_name = aws_key_pair.skp.key_name
-
-
-  
-  //key_name = "dbkp_tf"
+  key_name = aws_key_pair.kp.key_name
 
   # Attaching 2 security groups here, 1 for the MySQL Database access by the Web-servers,
   # & other one for the Bastion Host access for applying updates & patches!
-  vpc_security_group_ids = [aws_security_group.MySQL-SG.id, aws_security_group.DB-SG-SSH.id]
-  associate_public_ip_address = false
-
+  vpc_security_group_ids = [aws_security_group.MySQL-SG.id]
+  
   tags = {
    Name = "MySQL_Terraform"
   }
 
-  # user_data = <<EOF
-  #   #!/bin/bash
-  #    yum update -y
-  #    amazon-linux-extras install nginx1 -y 
-  #    systemctl enable nginx
-  #    systemctl start nginx
-	# EOF
-
 }
-
-
-# Creating an AWS instance for the Bastion Host, It should be launched in the public Subnet!
-resource "aws_instance" "Bastion-Host" {
-   depends_on = [
-     aws_instance.webserver,
-     aws_instance.MySQL,
-     #aws_subnet.public_us_east_1a,
-     #aws_subnet.public_us_east_1b
-  ]
-  
-  ami  = var.ami
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_us_east_1a.id
-
-  key_name = aws_key_pair.skp.key_name
-  
-  
-  //key_name = "MyKeyFinal"
-   
-  # Security group ID's
-  vpc_security_group_ids = [aws_security_group.BH-SG.id]
-  tags = {
-   Name = "Bastion_Host_Terraform"
-  }
-}
-
 
 
 
