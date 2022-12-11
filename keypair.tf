117a68d6766f9f865db683d4838c4b9bd42ba597
amena-aporna <amena.aporna@gmail.com>
2022-12-11 10:16:02 +0600

    changing key name

--- keypair.tf
+++ keypair.tf
@@ -1,13 +1,13 @@
 # Generating KeyPair 
-resource "tls_private_key" "keyp" {
+resource "tls_private_key" "prvtkey" {
   algorithm = "RSA"
 }
-resource "local_file" "mytfakey" {
-  content  = tls_private_key.keyp.private_key_pem
+resource "local_file" "key" {
+  content  = tls_private_key.prvtkey.private_key_pem
   filename = "kp_tf.pem"
 }
-resource "aws_key_pair" "skp" {
+resource "aws_key_pair" "kp" {
   key_name   = "kp_tf"
-  public_key = tls_private_key.keyp.public_key_openssh
+  public_key = tls_private_key.prvtkey.public_key_openssh
 }
 
