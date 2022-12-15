2c601e50f01847e51a6d041d1c2b2563cb252aba
amena-aporna <amena.aporna@gmail.com>
2022-12-15 12:45:41 +0600

    mysql ssh port added

--- security groups.tf
+++ security groups.tf
@@ -76,14 +76,27 @@ resource "aws_security_group" "MySQL-SG" {
   name = "mysql-sg"
   vpc_id = aws_vpc.main.id
 
-  # Created an inbound rule for MySQL
-  ingress {
-    description = "MySQL Access"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    security_groups = [aws_security_group.WS-SG.id]
+  # # Created an inbound rule for MySQL
+  # ingress {
+  #   description = "MySQL Access"
+  #   from_port   = 8080
+  #   to_port     = 8080
+  #   protocol    = "tcp"
+  #   security_groups = [aws_security_group.WS-SG.id]
     
+  # }
+
+  
+  # Created an inbound rule for SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+
+    # Here adding tcp instead of ssh, because ssh in part of tcp only!
+    protocol    = "tcp"
+    #cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.WS-SG.id]
   }
 
   egress {
